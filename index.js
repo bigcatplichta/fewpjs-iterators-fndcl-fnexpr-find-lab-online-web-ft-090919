b1f23f723ef24772afb8d54919070eb5608c6ed8
Matt Plichta <bigcatplichta@gmail.com>
2020-01-20 22:37:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ function testFunc() {
 }
 
 superbowlWin = (array) => {
+  // find element that has "W" for Object.result
   let result = array.find( e => e.result === "W")
+  // return year of win if result if found, else return undefined
   return !!result? result.year : undefined
 }
